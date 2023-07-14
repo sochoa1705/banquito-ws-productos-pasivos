5824f610b9ff4742d0472e5946f0b1840ea61769
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2023-07-14 14:08:32 -0500

    Spring Initializr

---
+++ src/main/java/ec/edu/espe/arquitectura/wsproductospasivos/WsProductosPasivosApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.wsproductospasivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProductosPasivosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsProductosPasivosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/espe/arquitectura/wsproductospasivos/WsProductosPasivosApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.wsproductospasivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsProductosPasivosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
